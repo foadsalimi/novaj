bfa0bd582342887a7ab418a3ff5b56427f947367
EVAIAhiqnhaijai <78456507+EVAIAhiqnhaijai@users.noreply.github.com>
2021-02-03 21:58:05 -0500

    Update ssrrmu.sh

--- ensayar/cgh/back/ssrrmu.sh
+++ ensayar/cgh/back/ssrrmu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo ""
-wget -q -O /tmp/ssr https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/msg-bar/msg 
+wget -q -O /tmp/ssr https://raw.githubusercontent.com/EVAIAhiqnhaijai/novaj/Master/ensayar/cgh/msg-bar/msg 
 cat /tmp/ssr > /tmp/ssrrmu.sh
-wget -q -O /tmp/ssr https://www.dropbox.com/s/1he3n0b7e4bpu0g/C-SSR.sh
+wget -q -O /tmp/ssr https://www.dropbox.com/s/8ouea8bnmc4n2mi/C-SSR.sh
 cat /tmp/ssr >> /tmp/ssrrmu.sh
 #curl  https://www.dropbox.com/s/re3lbbkxro23h4g/C-SSR.sh >> 
 sed -i "s;VPS•MX;ChumoGH-ADM;g" /tmp/ssrrmu.sh
